6c03bf0c6c38c6701a49f4df72995f0a4e9bd67d
Faisal Ahmed <faisal@ahmedmail.info>
2018-07-21 20:59:29 +0100

    AddressLocation bugfix

--- BusinessServices/ShoppingService/AddressLocations/AddressLocation.cs
+++ BusinessServices/ShoppingService/AddressLocations/AddressLocation.cs
@@ -5,11 +5,11 @@ namespace FMASolutionsCore.BusinessServices.ShoppingService
 {
     public class AddressLocation : AddressLocationEntity, IModel
     {
-        public AddressLocation(ICustomModelState modelState, int addressLocationID = 0, string adressLocationCode = "", int cityAreaID = 0, int postCodeID = 0, string addressLine1 = "", string addressLine2 = "")
+        public AddressLocation(ICustomModelState modelState, int addressLocationID = 0, string addressLocationCode = "", int cityAreaID = 0, int postCodeID = 0, string addressLine1 = "", string addressLine2 = "")
         {
             this.ModelState = modelState;
             this._addressLocationID = addressLocationID;
-            this._addressLocationCode = AddressLocationCode;
+            this._addressLocationCode = addressLocationCode;
             this._cityAreaID = cityAreaID;
             this._postCodeID = postCodeID;      
             this._addressLine1 = addressLine1;
