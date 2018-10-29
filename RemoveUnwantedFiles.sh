980a5d7d66319180545a8112f1826a22fcc05049
Faisal Ahmed <faisal@ahmedmail.info>
2018-10-29 21:22:57 +0000

    tidyup

--- RemoveUnwantedFiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#Remove All BIN+OBJ Folders to force remove any old binary object references
-find . -iname "bin" -type d | xargs rm -rf
-find . -iname "obj" -type d | xargs rm -rf
-find . -iname "Logs" -type d | xargs rm -rf
