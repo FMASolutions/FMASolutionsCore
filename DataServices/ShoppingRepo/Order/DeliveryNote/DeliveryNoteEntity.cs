a1fc1c7fb95c5e309f85dbd25edfd2fe302444ec
FMASolutions <faisal@ahmedmail.info>
2018-11-19 10:42:20 +0000

    Update DeliveryNoteEntity.cs

--- DataServices/ShoppingRepo/Order/DeliveryNote/DeliveryNoteEntity.cs
+++ DataServices/ShoppingRepo/Order/DeliveryNote/DeliveryNoteEntity.cs
@@ -1,1 +1,39 @@
+using System;
+using FMASolutionsCore.DataServices.DataRepository;
+using System.Collections.Generic;
 
+namespace FMASolutionsCore.DataServices.ShoppingRepo
+{
+    public class DeliveryNoteEntity : IBaseEntity
+    {
+        public class DeliveryNoteItem
+        {
+            public Int32 DeliveryNoteItemID {get; set;}
+            public Int32 DeliveryID {get; set;}
+            public Int32 OrderItemID {get; set;}
+        }
+        
+        public DeliveryNoteEntity()
+        {
+            _deliveryNoteItems = new List<DeliveryNoteItem>();
+            _deliveryDate = new DateTime();
+        }
+        public DeliveryNoteEntity(Int32 deliveryNoteID, Int32 orderHeaderID, DateTime deliveryDate, List<DeliveryNoteItem> items)
+        {
+            _deliveryNoteID = deliveryNoteID;
+            _orderHeaderID = orderHeaderID;
+            _deliveryDate = deliveryDate;
+            _deliveryNoteItems = items;
+        }
+
+        protected Int32 _deliveryNoteID;
+        protected Int32 _orderHeaderID;
+        protected DateTime _deliveryDate;
+        protected List<DeliveryNoteItem> _deliveryNoteItems;
+        
+        public Int32 ID { get { return _deliveryNoteID; } set { _deliveryNoteID = value; } }
+        public Int32 DeliveryNoteID { get { return _deliveryNoteID; } set { _deliveryNoteID = value; } }     
+        public Int32 OrderHeaderID { get { return _orderHeaderID;} set { _orderHeaderID = value; } }
+        public DateTime DeliveryDate { get { return _deliveryDate; } set { _deliveryDate = value; } }
+    }
+}
